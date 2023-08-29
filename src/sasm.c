3d7f03ebb553944a3ad8e4996a7232e3316e451a
Shidozz <shidozzmusic@gmail.com>
2023-08-29 16:18:17 +0200

    first commit

--- src/sasm.c
+++ src/sasm.c
@@ -38,9 +38,9 @@ int Sasm(const char *inputF, const char *outputF) {
     for (int i = 0; i < instructionCount; i++) {
         if (instructions[i].errOpcode == KW_ERROR){
             if(strcmp(instructions[i].operand1.value, "ERROR") == 0)
-                printf("Error: Bad token type used! This type of token (%s) can't be used in a %s opcode\n", TokenToString(instructions[i].operand1), KeywordToString(instructions[i].opcode));
+                printf("Error(%s : %d): Bad token type used! This type of token (%s) can't be used in a %s opcode\n", inputF, i, TokenToString(instructions[i].operand1), KeywordToString(instructions[i].opcode));
             if(strcmp(instructions[i].operand2.value, "ERROR") == 0)
-                printf("Error: Bad token type used! This type of token (%s) can't be used in a %s opcode\n", TokenToString(instructions[i].operand2), KeywordToString(instructions[i].opcode));
+                printf("Error(%s : %d): Bad token type used! This type of token (%s) can't be used in a %s opcode\n", inputF, i, TokenToString(instructions[i].operand2), KeywordToString(instructions[i].opcode));
             return 1;
         } else if(instructions[i].opcode == KW_RET){
             printf("Instruction %d: Opcode=%s\n", i, KeywordToString(instructions[i].opcode));
